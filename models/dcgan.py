c583f2492155787759b88c82fb2f15df0dff6d6f
Volodymyr Kuleshov <vol.kuleshov@gmail.com>
2017-02-08 18:25:41 -0800

    tweak it so it works better

--- models/dcgan.py
+++ models/dcgan.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 from keras.layers.convolutional import Convolution2D, Deconvolution2D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.core import Dense, Reshape, Flatten
+from keras.layers.core import Dense, Reshape, Flatten, Activation
 from keras.layers import Input
 from keras.models import Model
 
@@ -39,11 +39,13 @@ class DCGAN(object):
       x = Dense(n_g_hid2*7*7)(x)
       x = Reshape((n_g_hid2, 7, 7))(x)
       x = Deconvolution2D(64, 5, 5, output_shape=(128, 64, 14, 14), 
-            border_mode='same', activation='relu', subsample=(2,2), 
-            init='glorot_uniform', dim_ordering='th')(x)
+            border_mode='same', activation=None, subsample=(2,2), 
+            init='orthogonal', dim_ordering='th')(x)
+      x = BatchNormalization()(x)
+      x = Activation('relu')(x)
       g = Deconvolution2D(n_chan, 5, 5, output_shape=(128, n_chan, 28, 28), 
-            border_mode='same', activation='relu', subsample=(2,2), 
-            init='glorot_uniform', dim_ordering='th')(x)
+            border_mode='same', activation='sigmoid', subsample=(2,2), 
+            init='orthogonal', dim_ordering='th')(x)
 
     # create discriminator
     with tf.name_scope('discriminator'):
@@ -52,21 +54,21 @@ class DCGAN(object):
       x = Convolution2D(nb_filter=64, nb_row=5, nb_col=5, subsample=(2,2),
             activation=None, border_mode='same', init='glorot_uniform',
             dim_ordering='th')(Xk_d)
-      x = LeakyReLU(0.2)(x)
       x = BatchNormalization()(x)
+      x = LeakyReLU(0.2)(x)
 
       x = Convolution2D(nb_filter=128, nb_row=5, nb_col=5, subsample=(2,2),
             activation=None, border_mode='same', init='glorot_uniform',
             dim_ordering='th')(x)
-      x = LeakyReLU(0.2)(x)
       x = BatchNormalization()(x)
+      x = LeakyReLU(0.2)(x)
 
       x = Flatten()(x)
       x = Dense(1024)(x)
-      x = LeakyReLU(0.2)(x)
       x = BatchNormalization()(x)
+      x = LeakyReLU(0.2)(x)
 
-      d = Dense(1, activation='sigmoid')(x)
+      d = Dense(1, activation=None)(x)
 
     # save inputs
     X_g = tf.placeholder(tf.float32, shape=(None, n_lat), name='X_g')
@@ -91,28 +93,42 @@ class DCGAN(object):
     # create losses
     one  = np.array([[1]]).astype('float32')
     zero = np.array([[0]]).astype('float32')
-    self.loss_g = tf.reduce_mean(K.binary_crossentropy(d_fake, one))
-    self.loss_d = tf.reduce_mean(K.binary_crossentropy(d_real, one)) \
-                + tf.reduce_mean(K.binary_crossentropy(d_fake, zero))
+    
+    self.loss_g = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(d_fake, one))
+    self.loss_d = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(d_real, one)) \
+                + tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(d_fake, zero))
+    # self.loss_g = tf.reduce_mean(K.binary_crossentropy(d_fake, one))
+    # self.loss_d = tf.reduce_mean(K.binary_crossentropy(d_real, one)) \
+    #             + tf.reduce_mean(K.binary_crossentropy(d_fake, zero))
 
     # compute and store discriminator probabilities
-    self.d_real = tf.reduce_mean(d_real)
-    self.d_fake = tf.reduce_mean(d_fake)
-    self.p_real = tf.reduce_mean(tf.round(d_real))
-    self.p_fake = tf.reduce_mean(tf.round(d_fake))
+    self.d_real = tf.reduce_mean(tf.sigmoid(d_real))
+    self.d_fake = tf.reduce_mean(tf.sigmoid(d_fake))
+    self.p_real = tf.reduce_mean(tf.round(tf.sigmoid(d_real)))
+    self.p_fake = tf.reduce_mean(tf.round(tf.sigmoid(d_fake)))
 
     # create an optimizer
     lr = opt_params['lr']
-    optimizer_g = tf.train.AdamOptimizer(lr)
-    optimizer_d = tf.train.AdamOptimizer(lr)
+    optimizer_g = tf.train.AdamOptimizer(3e-4)
+    optimizer_d = tf.train.AdamOptimizer(3e-4)
+    optimizer   = tf.train.AdamOptimizer(3e-4)
 
     # get gradients
     gv_g = optimizer_g.compute_gradients(self.loss_g, w_g)
     gv_d = optimizer_d.compute_gradients(self.loss_d, w_d)
 
+    for g,v in gv_g:
+      print g, v
+
+    print 'discriminator:'
+
+    for g,v in gv_d:
+      print g, v
+
     # create training operation
     self.train_op_g = optimizer_g.apply_gradients(gv_g)
     self.train_op_d = optimizer_d.apply_gradients(gv_d)
+    self.train_op   = optimizer_d.apply_gradients(gv_d + gv_g)
 
   def fit(self, X_train, X_val, n_epoch=10, n_batch=128, logdir='dcgan-run'):
     # initialize log directory                  
@@ -144,15 +160,16 @@ class DCGAN(object):
         feed_dict = self.load_batch(X_batch, noise)
 
         # take training step
-        self.train_g(feed_dict)
-        self.train_d(feed_dict)
+        # self.train_g(feed_dict)
+        # self.train_d(feed_dict)
+        self.train(feed_dict)
 
       # log results at the end of batch
       tr_g_err, tr_d_err, tr_p_real, tr_p_fake = self.eval_err(X_train)
       va_g_err, va_d_err, va_p_real, va_p_fake = self.eval_err(X_val)
 
       print "Epoch {} of {} took {:.3f}s ({} minibatches)".format(
-        epoch + 1, n_epoch, time.time() - start_time, len(X_train) % n_batch)
+        epoch + 1, n_epoch, time.time() - start_time, len(X_train) // n_batch)
       print "  training loss/p_real/p_fake:\t\t{:.4f}\t{:.4f}\t{:.2f}\t{:.2f}".format(
         tr_g_err, tr_d_err, tr_p_real, tr_p_fake)
       print "  validation loss/p_real/p_fake:\t\t{:.4f}\t{:.4f}\t{:.2f}\t{:.2f}".format(
@@ -171,7 +188,8 @@ class DCGAN(object):
 
   def gen(self, noise):
     X_g_in, X_d_in = self.inputs
-    return self.sess.run(self.P, feed_dict={X_g_in : noise})
+    feed_dict = { X_g_in : noise, K.learning_phase() : False }
+    return self.sess.run(self.P, feed_dict=feed_dict)
 
   def train_g(self, feed_dict):
     _, loss_g = self.sess.run([self.train_op_g, self.loss_g], feed_dict=feed_dict)
@@ -180,6 +198,9 @@ class DCGAN(object):
   def train_d(self, feed_dict):
     _, loss_d = self.sess.run([self.train_op_d, self.loss_d], feed_dict=feed_dict)
     return loss_d
+
+  def train(self, feed_dict):
+    self.sess.run(self.train_op, feed_dict=feed_dict)
 
   def load_batch(self, X_train, noise, train=True):
     X_g_in, X_d_in = self.inputs
